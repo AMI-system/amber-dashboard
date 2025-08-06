8c39a7709287c24a4505a20e73d731dcb76c504b
KatrionaGoldmann <kgoldmann@turing.ac.uk>
2025-08-06 14:44:43 +0100

    add story for big moths

--- stories/stories.js
+++ stories/stories.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   const stories = [
     { file: './story_ml/story_ml.html', title: 'Story ML: The Beauty of Data' },
     { file: './story_encounters/story_encounters.html', title: 'Wildlife Encounters: Biodiversity in Action' },
-    { file: './story3/story3.html', title: 'Story 3: Unlocking Insights' },
+    { file: './story_big/story_big.html', title: 'Big Beautiful Moths: Nature\'s Giants' },
     { file: './story4/story4.html', title: 'Story 4: Data in Action' }, // Added 4th story
   ];
 
